5b662af63552bb9f1840a2d30654ceaee636af43
yuichiro-h <u1row.h@gmail.com>
2014-10-17 23:33:53 +0900

    v0.2.0

--- ls-ec2.rb
+++ ls-ec2.rb
@@ -2,14 +2,14 @@ require "formula"
 
 class LsEc2 < Formula
   homepage "https://github.com/yuichiro-h/ls-ec2"
-  version 'v0.1.0'
+  version 'v0.2.0'
 
   if Hardware.is_64_bit?
-    url "https://github.com/yuichiro-h/ls-ec2/releases/download/v0.1.0/ls-ec2_0.1.0_darwin_amd64.zip"
-    sha1 "aa94f63335f949463f70034d6a25a422d6339506"
+    url "https://github.com/yuichiro-h/ls-ec2/releases/download/v0.2.0/ls-ec2_0.2.0_darwin_amd64.zip"
+    sha1 "92b1f8482f1f4e280962be7f24bd9e8b15c76f0f"
   else
-    url "https://github.com/yuichiro-h/ls-ec2/releases/download/v0.1.0/ls-ec2_0.1.0_darwin_386.zip"
-    sha1 "3472f30d4a78349d30d42557182876f21f357cec"
+    url "https://github.com/yuichiro-h/ls-ec2/releases/download/v0.2.0/ls-ec2_0.2.0_darwin_386.zip"
+    sha1 "f88e711076dddf624a641b05ebf32fcbb7ce69ce"
   end
 
   depends_on :arch => :intel
