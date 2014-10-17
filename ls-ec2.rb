604a92189b726640f7cfe8b20fa65e2875e7994e
yuichiro-h <u1row.h@gmail.com>
2014-10-17 23:55:48 +0900

    update

--- ls-ec2.rb
+++ ls-ec2.rb
@@ -6,10 +6,10 @@ class LsEc2 < Formula
 
   if Hardware.is_64_bit?
     url "https://github.com/yuichiro-h/ls-ec2/releases/download/v0.2.0/ls-ec2_0.2.0_darwin_amd64.zip"
-    sha1 "92b1f8482f1f4e280962be7f24bd9e8b15c76f0f"
+    sha1 "27ae6f10bc04802fe42d6e510d4be430db21121a"
   else
     url "https://github.com/yuichiro-h/ls-ec2/releases/download/v0.2.0/ls-ec2_0.2.0_darwin_386.zip"
-    sha1 "f88e711076dddf624a641b05ebf32fcbb7ce69ce"
+    sha1 "e6b54d1142d9c572d939f2cf391dc7ea545ea040"
   end
 
   depends_on :arch => :intel
