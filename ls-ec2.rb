e7da6347b7f0c550b24df5d14b47de7138cfa2fe
yuichiro-h <u1row.h@gmail.com>
2016-11-18 15:46:44 +0900

    0.2.1

--- ls-ec2.rb
+++ ls-ec2.rb
@@ -1,20 +1,39 @@
-require "formula"
+# Documentation: https://github.com/Homebrew/brew/blob/master/docs/Formula-Cookbook.md
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 
 class LsEc2 < Formula
-  homepage "https://github.com/yuichiro-h/ls-ec2"
-  version 'v0.2.0'
+  desc ""
+  homepage ""
+  url "https://github.com/yuichiro-h/ls-ec2/archive/v0.2.1.tar.gz"
+  version "0.2.1"
+  sha256 "4449c27948a8a5974e255635e70a0599ae4100dbeffb594f75c5a1959775e713"
 
-  if Hardware.is_64_bit?
-    url "https://github.com/yuichiro-h/ls-ec2/releases/download/v0.2.0/ls-ec2_0.2.0_darwin_amd64.zip"
-    sha1 "27ae6f10bc04802fe42d6e510d4be430db21121a"
-  else
-    url "https://github.com/yuichiro-h/ls-ec2/releases/download/v0.2.0/ls-ec2_0.2.0_darwin_386.zip"
-    sha1 "e6b54d1142d9c572d939f2cf391dc7ea545ea040"
-  end
-
-  depends_on :arch => :intel
+  # depends_on "cmake" => :build
+  depends_on :x11 # if your formula requires any X11/XQuartz components
 
   def install
-    bin.install 'ls-ec2'
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test ls-ec2`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
   end
 end
